a23447f813188bea4d3572c8cdb0821d3c37391d
Gareth <gclapperton@live.com>
2015-05-13 15:24:17 -0400

    Updated dependencies and bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject views "1.4.0-SNAPSHOT"
+(defproject views "1.4.1-SNAPSHOT"
   :description "A view to the past helps navigate the future."
 
   :url "https://github.com/kirasystems/views"
@@ -7,17 +7,18 @@
             :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/tools.logging "0.3.1"]
-                 [clj-logging-config "1.9.10"]
-                 [prismatic/plumbing "0.3.5"]
-                 [pjstadig/humane-test-output "0.6.0"]
+                 [clj-logging-config "1.9.12"]
+                 [prismatic/plumbing "0.4.3"]
+                 [pjstadig/humane-test-output "0.7.0"]
                  [environ "1.0.0"]]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :test {:dependencies [[org.clojure/tools.nrepl "0.2.3"]
+             :test {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
                                    [environ "1.0.0"]
                                    [org.clojure/data.generators "0.1.2"]]
 
                     :injections [(require 'pjstadig.humane-test-output)
                                  (pjstadig.humane-test-output/activate!)]}}
 
-  :plugins [[lein-environ "0.4.0"]])
+  :plugins [[lein-ancient "0.6.7"]
+            [lein-environ "0.4.0"]])
