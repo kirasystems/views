d15270b8b053e13aec61d09a67a3d4852277c061
Alexander Hudek <alex@kirasystems.com>
2016-08-11 16:24:51 -0400

    Version 1.5.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/views "1.5.0"
+(defproject kirasystems/views "1.5.1-SNAPSHOT"
   :description "A view to the past helps navigate the future."
 
   :url "https://github.com/kirasystems/views"
