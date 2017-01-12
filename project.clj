1d4269ba04cc8667f3560dd700d520869ee1b141
jpgauthier <j-p.g@hotmail.com>
2017-01-12 15:14:07 -0500

    Version 1.5.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/views "1.5.1"
+(defproject kirasystems/views "1.5.2-SNAPSHOT"
   :description "A view to the past helps navigate the future."
 
   :url "https://github.com/kirasystems/views"
