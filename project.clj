b8b6b6a0601257e75b68d6fb776d03a1c294ac13
jpgauthier <j-p.g@hotmail.com>
2017-01-12 15:14:06 -0500

    Version 1.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/views "1.5.1-SNAPSHOT"
+(defproject kirasystems/views "1.5.1"
   :description "A view to the past helps navigate the future."
 
   :url "https://github.com/kirasystems/views"
