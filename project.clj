aea091c549516b1b0e0a198be2d6c30661916073
Alexander Hudek <alex@kirasystems.com>
2017-08-11 17:56:08 -0400

    DEV-497 Update version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/views "2.0.0-SNAPSHOT"
+(defproject kirasystems/views "2.0.0"
   :description   "A view to the past helps navigate the future."
   :url           "https://github.com/kirasystems/views"
 
