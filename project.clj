76af329e41e4dfca4c026c0ca703c79568def5ef
Charlie Wang <charlie.wang@kirasystems.com>
2017-11-16 10:50:25 -0500

    Version 2.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/views "2.0.1-SNAPSHOT"
+(defproject kirasystems/views "2.0.1"
   :description   "A view to the past helps navigate the future."
   :url           "https://github.com/kirasystems/views"
 
