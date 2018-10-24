16746454cf32041b70ed269b7836290dabbc0bb1
Yana Lebedeva <kollapse@gmail.com>
2018-10-23 23:31:07 -0400

    Version 2.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/views "2.0.2"
+(defproject kirasystems/views "2.0.3-SNAPSHOT"
   :description   "A view to the past helps navigate the future."
   :url           "https://github.com/kirasystems/views"
 
